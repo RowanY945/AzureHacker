b9007fe548cf36373c58c178c100e0faff15e4a5
RuiwenYang <ruiweny96@gmail.com>
2024-11-16 14:57:22 +1100

    create

---
+++ backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloWorldController : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult<string> HelloWorld()
+    {
+        var name = "Patrick";
+        return $"Hello world {name}";
+    }
+}
